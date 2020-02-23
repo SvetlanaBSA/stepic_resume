47d3e9537c18be82f65cdf769f0a8606f7f4993d
SvetlanaBSA <42421614+svetlanabsa@users.noreply.github.com>
2020-02-23 23:53:33 +0300

    test_main_page added

--- conftest.py
+++ conftest.py
@@ -1,25 +1,34 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default="en",
                      help="Choose language: en or ru")
+    parser.addoption('--browser_name', action='store', default="chrome",
+                     help="Choose browser: chrome or firefox")
 
 
 @pytest.fixture(scope="function")
-def browser(request):
-    language = request.config.getoption("--language")
-    browser = webdriver.Chrome()
+def language(request):
+    language = request.config.getoption("language")
     if language == "en":
         print("\nstart browser with en language for test..")
     elif language == "ru":
         print("\nstart browser with ru language for test..")
     else:
         raise pytest.UsageError("--language should be en or ru")
-    if language == "es" or language == "fr":
-        link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
+    if language == "en" or language == "ru":
+        link = f"http://selenium1py.pythonanywhere.com/{language}"
         browser.get(link)
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
     yield browser
     print("\nquit browser..")
     browser.quit()
